ec089c855bda9a473bd86dd631f673e9f08a98c2
Lihkin <shniks@gmail.com>
2018-02-11 10:59:49 -0700

    Creating responder class and associated tests.

---
+++ lib/responder.rb
@@ -0,0 +1,28 @@
+require './lib/request_formatter'
+require './lib/root'
+require './lib/hello_world'
+require './lib/datetime'
+require './lib/word_search'
+require './lib/game'
+require './lib/shutdown'
+
+class Responder
+
+  def initialize
+    @request_formatter = RequestFormatter.new
+  end
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+end
