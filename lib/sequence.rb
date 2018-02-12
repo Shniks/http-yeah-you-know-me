6ce989a8267fa91f9aa4ed8939622f7d84b3f6ca
Lihkin <shniks@gmail.com>
2018-02-11 17:43:15 -0700

    Adding Sequence class.

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,21 +1,39 @@
 require './lib/server'
 require './lib/request_formatter'
+require './lib/responder'
+require './lib/game'
 
 class Sequence
 
   def start
     server = Server.new(9292)
-    get_path
+    request_formatter = RequestFormatter.new
+    request_count = 0
+    hello_count = 0
+
+    puts "\nReady for a request...\n(Press 'ctrl + c' at any time to exit)\n "
+
+    loop do
+      server.accept_request
+      request_lines = server.request_from_client
+      path = request_formatter.path(request_lines)
+      responder = Responder.new
+
+      request_count += 1
+      hello_count += 1 if path == "/hello"
+
+      response = responder.response_created(request_lines, request_count, hello_count)
+      server.response_from_server(response)
+
+      if path == "/start_game" && request_formatter.verb(request_lines) == "POST"
+        game = Game.new(server)
+        game.run
+      end
+
+      break if path == "/shutdown"
+    end
+
+    server.close_the_server
   end
-
-  def get_path
-    RequestFormatter.new.path(request_lines)
-  end
-
-  
-
-
-
-
 
 end
